2d1ec8542af1985cac9e730222f77ab9d5f7f178
APOLLO <mgmstorenoida@gmail.com>
2025-11-16 17:01:38 +0530

    updated backend url

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,2 +1,2 @@
 // Simple API configuration
-export const VITE_API_BASE_URL = "https://userdatamanagement-393167800506.europe-west1.run.app"; 
+export const VITE_API_BASE_URL = "https://user-data-management-704104946630.europe-west1.run.app"; 
